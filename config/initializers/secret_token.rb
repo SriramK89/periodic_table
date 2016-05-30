9880f75ec9fe645d345a2897fcb501cbece07417
Sriram Kumar <ksrirambecse@gmail.com>
2016-05-30 14:56:03 +0530

    Completed changing from Rails to HTML

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-PeriodicTable::Application.config.secret_token = 'f3c83634dc5261b307fae63e4944d3e774a51093b4e2a24fdb042871dde34de554d8683e6237a46a330e7d2dd3435661c5c73e891fbdd902addc6b8153508ba8'
